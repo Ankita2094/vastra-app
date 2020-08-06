29117f164ce578486237cf42687db4e9931b8dcd
Ankita2094 <ankitaktiwari7@gmail.com>
2020-08-06 15:33:12 -0400

    Vastra Angular Web Application

--- vastra-backend/database/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://tiwariank:Anki@2029@cluster0.tadby.mongodb.net/Catalog?retryWrites=true&w=majority/Catalog', { useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify : false})
-.then(() => console.log("Db Connected")).catch((error) => console.log(error));
-
-
-
-module.exports = mongoose;
