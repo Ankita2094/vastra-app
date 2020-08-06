7a40aedd1b44a51947d0e34eb6521713b74a0400
Ankita Tiwari <ankitaktiwari7@gmail.com>
2020-08-06 14:56:12 -0400

    Update mongoose.js

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://tiwariank:Anki@2029@cluster0.tadby.mongodb.net/Catalog?retryWrites=true&w=majority/Catalog', { useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify : false})
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.tadby.mongodb.net/Catalog?retryWrites=true&w=majority/Catalog', { useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify : false})
 .then(() => console.log("Db Connected")).catch((error) => console.log(error));
 
 
